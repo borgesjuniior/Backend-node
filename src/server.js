ac83906e936fd79c56f4231b096de88a5bbe41c7
Júnior <valdecyborgesjr@hotmail.com>
2020-12-20 14:44:07 -0300

    Adicionados métodos HTTP

--- src/server.js
+++ src/server.js
@@ -4,12 +4,35 @@ const app = express()
 
 
 
-app.get('/', (req, res) => {
-    res.json({
-        "hello": "Hello world 2"
-    });
+app.get('/projects', (req, res) => {
+    res.json([
+        'Projeto 1',
+        'projeto 2'
+    ]);
 });
 
+app.post('/projects', (req, res) => {
+    res.json([
+        'Projeto 1',
+        'projeto 2',
+        'projeto 3',
+    ]);
+});
+
+app.put('/projects', (req, res) => {
+    res.json([
+        'Projeto 4',
+        'projeto 2',
+        'projeto 3',
+    ]);
+});
+
+app.delete('/projects', (req, res) => {
+    res.json([
+        'Projeto 1',
+        'projeto 2',
+    ]);
+});
 
 app.listen(3333, () => {
     console.log('Server has started!')
