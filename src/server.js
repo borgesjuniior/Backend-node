7627b0a917b1a80daf8ccff73d807e7dd3a52311
Júnior <valdecyborgesjr@hotmail.com>
2020-12-20 19:23:11 -0300

    Adição do midleware

--- src/server.js
+++ src/server.js
@@ -1,15 +1,44 @@
+const { request } = require('express');
 const express = require ('express');
 const app = express()
 const { uuid } = require('uuidv4')
 
 
 app.use(express.json())
+app.use(logRequest); //Aplica o midleware em todas as rotas da aplicação 
 
+/**
+ * Métodos HTTP.
+ * GET: Busca informações do backend
+ * POST: Cria uma informação no backend
+ * PUT/PATH: Altera uma informação no backend
+ * DELETE: Deleta uma informação no backend
+ */
 
+/**
+ * Tipos de parâmetros
+ * Query params: Filtros e páginação
+ * Route params: Indentificar os recursos (Alterar/deletar)
+ * Request body: Conteúdo na hora de criar ou editar um recurso (JSON)
+ */
+
+/**
+ * Midlewares. 
+ * Interceptador de requisições que pode interceptar/interromper dados da requisição
+ */
+
+function logRequest(req, res, next) {
+    const { method, url } = req;
+
+    const logLabel = `[${method}]: ${url}`;
+    console.log(logLabel)
+    next()
+
+}
 
 const projects = []
 
-app.get('/projects', (req, res) => res.json(projects));
+app.get('/projects', (req, res) => res.json(projects)); //O midleware pode ser passado de forma especifica para uma rota antes do req, res
 
 app.post('/projects', (req, res) => {
 
@@ -37,7 +66,7 @@ app.put('/projects/:id', (req, res) => {
         owner,
     }
 
-    projects[projectIndex] = project;
+    projects[projectIndex] = project; //Recria a informação dentro de projects 
 
     return res.json(project)
 
@@ -46,13 +75,13 @@ app.put('/projects/:id', (req, res) => {
 app.delete('/projects/:id', (req, res) => {
     const { id } = req.params;
 
-    const projectIndex = projects.findIndex(project => project.id == id);
+    const projectIndex = projects.findIndex(project => project.id == id); //Busca a posição do Id no array
 
     if (projectIndex < 0) {
         return res.status(400).json({"error": "Projet not found"})
     }
 
-    projects.splice(projectIndex, 1);
+    projects.splice(projectIndex, 1); //Deleta a informação no array
 
     res.status(204).send();
 
