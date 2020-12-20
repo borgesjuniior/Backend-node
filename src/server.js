8839139a75604ecf75fdd1e8abe14499bcc2e596
Júnior <valdecyborgesjr@hotmail.com>
2020-12-20 16:22:26 -0300

    Finalizando rotas da aplicação

--- src/server.js
+++ src/server.js
@@ -1,37 +1,61 @@
 const express = require ('express');
 const app = express()
+const { uuid } = require('uuidv4')
+
+
+app.use(express.json())
 
 
 
+const projects = []
 
-app.get('/projects', (req, res) => {
-    res.json([
-        'Projeto 1',
-        'projeto 2'
-    ]);
-});
+app.get('/projects', (req, res) => res.json(projects));
 
 app.post('/projects', (req, res) => {
-    res.json([
-        'Projeto 1',
-        'projeto 2',
-        'projeto 3',
-    ]);
+
+    const { title, owner } = req.body;
+    const project = {id: uuid(), title, owner}
+
+    projects.push(project)
+
+    return res.json(project)
 });
 
-app.put('/projects', (req, res) => {
-    res.json([
-        'Projeto 4',
-        'projeto 2',
-        'projeto 3',
-    ]);
+app.put('/projects/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, owner} = req.body;
+
+    const projectIndex = projects.findIndex(project => project.id == id);
+
+    if (projectIndex < 0) {
+        return res.status(400).json({"error": "Projet not found"})
+    }
+
+    const project = {
+        id,
+        title,
+        owner,
+    }
+
+    projects[projectIndex] = project;
+
+    return res.json(project)
+
 });
 
-app.delete('/projects', (req, res) => {
-    res.json([
-        'Projeto 1',
-        'projeto 2',
-    ]);
+app.delete('/projects/:id', (req, res) => {
+    const { id } = req.params;
+
+    const projectIndex = projects.findIndex(project => project.id == id);
+
+    if (projectIndex < 0) {
+        return res.status(400).json({"error": "Projet not found"})
+    }
+
+    projects.splice(projectIndex, 1);
+
+    res.status(204).send();
+
 });
 
 app.listen(3333, () => {
